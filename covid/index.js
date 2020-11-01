fbbda59498f2e22dff2bb19573add676e2482820
Sanskriti Negi <60841880+sanskritinegi@users.noreply.github.com>
2020-11-01 10:09:32 +0530

    Update index.js

--- covid/index.js
+++ covid/index.js
@@ -265,3 +265,37 @@ result
     }
   });
 });
+
+
+ //angularjs
+
+ var app=angular.module('myApp',['ngTouch']);
+    app.directive('ngCarousel', function() {
+      return function(scope, element, attrs) {
+        var el = element[0];
+        var containerEl = el.querySelector("ul");
+        var slidesEl = containerEl.querySelectorAll("li");
+        scope.numSlides = slidesEl.length;
+        scope.curSlide = 1;   
+        scope.$watch('curSlide', function(num) {
+          num = (num % scope.numSlides) + 1;
+          containerEl.style.left = (-1*100*(num-1)) + '%';
+        });
+        
+        el.style.position = 'relative';
+        el.style.overflow = 'hidden';
+
+        containerEl.style.position = 'absolute';
+        containerEl.style.width = (scope.numSlides*100)+'%';
+        containerEl.style.listStyleType = 'none';
+        containerEl.style.margin =0;
+        containerEl.style.padding=0;
+        containerEl.style.transition = '1s';
+        
+        for(var i=0; i<slidesEl.length; i++) {
+          var slideEl = slidesEl[i];
+          slideEl.style.display = 'inline-block';
+          slideEl.style.width = (100/scope.numSlides) + '%';
+        }
+      };
+    });
